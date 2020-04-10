89fcb72989b86e810db878f21b59d74c2033efa8
Renato Diniz <dinizre@hotmail.com>
2020-04-10 09:28:49 -0300

    Meal por categorias e MEal Item

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../components/meal_item.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
 
@@ -9,12 +11,19 @@ class CategoriesMealsScreen extends StatelessWidget {
 
     final category = ModalRoute.of(context).settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title)
       ),
-      body: Center(
-        child: Text('Receitas por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(categoryMeals[index]);
+        },
       ),
     );
   }
